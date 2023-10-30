343aba2a3d767fd4827c64e441147fa1ba4517a3
GabrielAbad <125308525+GabrielAbad@users.noreply.github.com>
2023-10-30 20:58:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from products import *
 from salesoperation import *
 
-#TODO - testar o codiguinho, usar as brands enumeradas
 
 kop_bar = Chocolate(45.32,"A01392","Kopenhagen","Brasil","Tablete Clássico",42,100)
 
-
 my_shoe = Shoe(450.99,"S01221",42,"Puma")
-
 
 my_bottle = Bottle(99.99,"P03444","Termolar",1000)
     
